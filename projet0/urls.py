641a169b20ca720b66bad513264ae1611fbc6e0e
GOHOUEDE <gohouedemahugnonaimeloick@gmail.com>
2023-09-11 11:46:10 +0100

    first commit

---
+++ projet0/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path ,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('home.urls')),
+    path('Blog/',include('Blog.urls'))
+]
